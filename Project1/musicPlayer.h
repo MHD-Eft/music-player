e8661aae94d651a76fb33dacde0a5de6432cdb08
Mahdieh <mahdieh.eftekharian@gmail.com>
2021-06-24 16:12:28 +0430

    Music Player Play-pause-hide-show are aded

--- Project1/musicPlayer.h
+++ Project1/musicPlayer.h
@@ -46,6 +46,25 @@ namespace Project1 {
 	private: System::Windows::Forms::Label^ lblLogo;
 	private: System::Windows::Forms::OpenFileDialog^ openFileDialog1;
 	private: System::Windows::Forms::SaveFileDialog^ saveFileDialog1;
+	private: System::Windows::Forms::GroupBox^ groupBoxMusic;
+
+	private: System::ComponentModel::BackgroundWorker^ backgroundWorker1;
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	private: System::Windows::Forms::ContextMenuStrip^ contextMenuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^ playToolStripMenuItem;
+
+	private: System::Windows::Forms::ToolStripMenuItem^ hideToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ pauseToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ hideToolStripMenuItem1;
+	private: System::Windows::Forms::ToolStripMenuItem^ showToolStripMenuItem;
+
+	private: System::ComponentModel::IContainer^ components;
+
+
+
+
+
+
 
 
 	protected:
@@ -54,7 +73,7 @@ namespace Project1 {
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -63,6 +82,7 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(musicPlayer::typeid));
 			this->axWindowsMediaPlayer1 = (gcnew AxWMPLib::AxWindowsMediaPlayer());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
@@ -72,18 +92,29 @@ namespace Project1 {
 			this->listBoxSongs = (gcnew System::Windows::Forms::ListBox());
 			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->saveFileDialog1 = (gcnew System::Windows::Forms::SaveFileDialog());
+			this->groupBoxMusic = (gcnew System::Windows::Forms::GroupBox());
+			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->playToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->hideToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->pauseToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->hideToolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->showToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->contextMenuStrip1 = (gcnew System::Windows::Forms::ContextMenuStrip(this->components));
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->axWindowsMediaPlayer1))->BeginInit();
 			this->panel1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->groupBoxMusic->SuspendLayout();
+			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// axWindowsMediaPlayer1
 			// 
 			this->axWindowsMediaPlayer1->Enabled = true;
-			this->axWindowsMediaPlayer1->Location = System::Drawing::Point(2, 51);
+			this->axWindowsMediaPlayer1->Location = System::Drawing::Point(6, 151);
 			this->axWindowsMediaPlayer1->Name = L"axWindowsMediaPlayer1";
 			this->axWindowsMediaPlayer1->OcxState = (cli::safe_cast<System::Windows::Forms::AxHost::State^>(resources->GetObject(L"axWindowsMediaPlayer1.OcxState")));
-			this->axWindowsMediaPlayer1->Size = System::Drawing::Size(523, 45);
+			this->axWindowsMediaPlayer1->Size = System::Drawing::Size(227, 45);
 			this->axWindowsMediaPlayer1->TabIndex = 0;
 			// 
 			// panel1
@@ -91,9 +122,9 @@ namespace Project1 {
 			this->panel1->BackColor = System::Drawing::Color::DarkTurquoise;
 			this->panel1->Controls->Add(this->lblLogo);
 			this->panel1->Controls->Add(this->pictureBox1);
-			this->panel1->Location = System::Drawing::Point(2, 4);
+			this->panel1->Location = System::Drawing::Point(6, 0);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(523, 42);
+			this->panel1->Size = System::Drawing::Size(227, 42);
 			this->panel1->TabIndex = 1;
 			// 
 			// lblLogo
@@ -122,9 +153,9 @@ namespace Project1 {
 			this->btnSelectSongs->BackColor = System::Drawing::Color::PeachPuff;
 			this->btnSelectSongs->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->btnSelectSongs->ForeColor = System::Drawing::Color::SaddleBrown;
-			this->btnSelectSongs->Location = System::Drawing::Point(529, 75);
+			this->btnSelectSongs->Location = System::Drawing::Point(6, 122);
 			this->btnSelectSongs->Name = L"btnSelectSongs";
-			this->btnSelectSongs->Size = System::Drawing::Size(120, 23);
+			this->btnSelectSongs->Size = System::Drawing::Size(227, 23);
 			this->btnSelectSongs->TabIndex = 3;
 			this->btnSelectSongs->Text = L"Select Songs";
 			this->btnSelectSongs->UseVisualStyleBackColor = false;
@@ -133,9 +164,9 @@ namespace Project1 {
 			// listBoxSongs
 			// 
 			this->listBoxSongs->FormattingEnabled = true;
-			this->listBoxSongs->Location = System::Drawing::Point(529, 2);
+			this->listBoxSongs->Location = System::Drawing::Point(6, 47);
 			this->listBoxSongs->Name = L"listBoxSongs";
-			this->listBoxSongs->Size = System::Drawing::Size(120, 69);
+			this->listBoxSongs->Size = System::Drawing::Size(227, 69);
 			this->listBoxSongs->TabIndex = 4;
 			this->listBoxSongs->SelectedIndexChanged += gcnew System::EventHandler(this, &musicPlayer::ListBoxSongs_SelectedIndexChanged);
 			// 
@@ -143,24 +174,92 @@ namespace Project1 {
 			// 
 			this->openFileDialog1->FileName = L"openFileDialog1";
 			// 
+			// groupBoxMusic
+			// 
+			this->groupBoxMusic->BackColor = System::Drawing::Color::OldLace;
+			this->groupBoxMusic->Controls->Add(this->panel1);
+			this->groupBoxMusic->Controls->Add(this->listBoxSongs);
+			this->groupBoxMusic->Controls->Add(this->axWindowsMediaPlayer1);
+			this->groupBoxMusic->Controls->Add(this->btnSelectSongs);
+			this->groupBoxMusic->Location = System::Drawing::Point(0, 27);
+			this->groupBoxMusic->Name = L"groupBoxMusic";
+			this->groupBoxMusic->Size = System::Drawing::Size(239, 208);
+			this->groupBoxMusic->TabIndex = 5;
+			this->groupBoxMusic->TabStop = false;
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->playToolStripMenuItem });
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(239, 24);
+			this->menuStrip1->TabIndex = 6;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// playToolStripMenuItem
+			// 
+			this->playToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
+				this->hideToolStripMenuItem,
+					this->pauseToolStripMenuItem, this->hideToolStripMenuItem1, this->showToolStripMenuItem
+			});
+			this->playToolStripMenuItem->Name = L"playToolStripMenuItem";
+			this->playToolStripMenuItem->Size = System::Drawing::Size(131, 20);
+			this->playToolStripMenuItem->Text = L"Music Player Settings";
+			// 
+			// hideToolStripMenuItem
+			// 
+			this->hideToolStripMenuItem->Name = L"hideToolStripMenuItem";
+			this->hideToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->hideToolStripMenuItem->Text = L"Play";
+			this->hideToolStripMenuItem->Click += gcnew System::EventHandler(this, &musicPlayer::hideToolStripMenuItem_Click);
+			// 
+			// pauseToolStripMenuItem
+			// 
+			this->pauseToolStripMenuItem->Name = L"pauseToolStripMenuItem";
+			this->pauseToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->pauseToolStripMenuItem->Text = L"Pause";
+			this->pauseToolStripMenuItem->Click += gcnew System::EventHandler(this, &musicPlayer::pauseToolStripMenuItem_Click);
+			// 
+			// hideToolStripMenuItem1
+			// 
+			this->hideToolStripMenuItem1->Name = L"hideToolStripMenuItem1";
+			this->hideToolStripMenuItem1->Size = System::Drawing::Size(180, 22);
+			this->hideToolStripMenuItem1->Text = L"Hide";
+			this->hideToolStripMenuItem1->Click += gcnew System::EventHandler(this, &musicPlayer::hideToolStripMenuItem1_Click);
+			// 
+			// showToolStripMenuItem
+			// 
+			this->showToolStripMenuItem->Name = L"showToolStripMenuItem";
+			this->showToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->showToolStripMenuItem->Text = L"Show";
+			this->showToolStripMenuItem->Click += gcnew System::EventHandler(this, &musicPlayer::showToolStripMenuItem_Click);
+			// 
+			// contextMenuStrip1
+			// 
+			this->contextMenuStrip1->Name = L"contextMenuStrip1";
+			this->contextMenuStrip1->Size = System::Drawing::Size(61, 4);
+			// 
 			// musicPlayer
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::Cornsilk;
-			this->ClientSize = System::Drawing::Size(653, 101);
-			this->Controls->Add(this->listBoxSongs);
-			this->Controls->Add(this->btnSelectSongs);
-			this->Controls->Add(this->panel1);
-			this->Controls->Add(this->axWindowsMediaPlayer1);
+			this->ClientSize = System::Drawing::Size(239, 238);
+			this->Controls->Add(this->groupBoxMusic);
+			this->Controls->Add(this->menuStrip1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"musicPlayer";
 			this->Text = L"Music Player";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->axWindowsMediaPlayer1))->EndInit();
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->groupBoxMusic->ResumeLayout(false);
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -185,74 +284,50 @@ private: System::Void BtnSelectSongs_Click(System::Object^ sender, System::Event
 		{
 			listBoxSongs->Items->Add(fileName[i]);
 		}
-
-		//listBoxSongs->Text = openFileDialog1->FileName;
-		//listBoxSongPath->Text=openFileDialog1->
-		/*
-		fileName = ofd.SafeFileNames; // save the name of track in filename array
-		paths = ofd.FileNames; // save the paths of the tracks in path array
-
-		for (int i = 0; i < fileName.Length; i++)
-		{
-			listBoxSongs.Items.Add(fileName[i]);
-		}
-
-
-		  if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-			{
-				fileName = ofd.SafeFileNames; // save the name of track in filename array
-				paths = ofd.FileNames; // save the paths of the tracks in path array
-
-				for (int i = 0; i < fileName.Length; i++)
-				{
-					listBoxSongs.Items.Add(fileName[i]);
-				}
-			}
-
-	}
-
-	if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
-	{
-		for each (auto path in  openFileDialog1->FileNames)
-		{
-			if (System::IO::Path::GetExtension(path) == ".txt")
-			{
-				listBoxSongs->Text += System::IO::Path::GetExtension(path);
-
-
-
-			}
-
-		}
-
-		*/
-		//String ^ filename = openFileDialog1->FileName;
-		//MessageBox::Show(filename);
-
-		/*String^ ex = System::IO::Path::GetExtension(filename);
-		if (ex == "txt") {}*/
-
-		/*
-		auto ex = System::IO::Directory::GetFiles("D:\\Mahdieh\\university\\Term2");
-		//MessageBox::Show(ex[0]);
-		for each (auto var in ex)
-		{
-			richTextBox1->Text += var + "\n";
-		}
-		*/
-
-
 	}
 	else
 	{
 	}
 
-
-
-
 }
 private: System::Void ListBoxSongs_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
 	axWindowsMediaPlayer1->URL = paths[listBoxSongs->SelectedIndex];
+}
+
+	   void Play()
+	   {
+		   axWindowsMediaPlayer1->Ctlcontrols->play();
+	   }
+	   void Pause()
+	   {
+		   axWindowsMediaPlayer1->Ctlcontrols->pause();
+	   }
+	   void hide()
+	   {
+		   axWindowsMediaPlayer1->Ctlcontrols->pause();
+		   groupBoxMusic->Hide();
+	   }
+
+	   void show()
+	   {
+		   axWindowsMediaPlayer1->Ctlcontrols->play();
+		   groupBoxMusic->Show();
+	   }
+
+
+
+
+private: System::Void hideToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+	Play();
+}
+private: System::Void pauseToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+	Pause();
+}
+private: System::Void hideToolStripMenuItem1_Click(System::Object^ sender, System::EventArgs^ e) {
+	hide();
+}
+private: System::Void showToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+	show();
 }
 };
 }
